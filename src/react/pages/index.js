ec303a1e939872d4d3c6703af9dcdededb9ef545
Malik M Alnakhaleh <jimalik2525@gmail.com>
2020-01-07 17:25:17 -0500

    Added LoginPage.js and LoginForm.js, almost done with outlining both

--- src/react/pages/index.js
+++ src/react/pages/index.js
@@ -1,8 +1,10 @@
 //import pages here
 import Home from "./Home";
+import LoginPage from "./LoginPage"
 
 
 //export the page here with a path and the component name
 export default {
-  Home: { path: "/", component: Home }
+  Home: { path: "/", component: Home },
+  LoginPage: { path: "/login", component: LoginPage }
 };
