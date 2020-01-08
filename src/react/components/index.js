d798bb33db00127211285ed68390e079ab177b7d
dbmiddle <dbmiddle39@gmail.com>
2020-01-08 11:56:42 -0500

    created customer card component

--- src/react/components/index.js
+++ src/react/components/index.js
@@ -1,6 +1,7 @@
 //component exports
 export { default as LoginForm } from "./LoginForm/LoginForm.js";
 export { default as NavBar } from "./NavBar/NavBar.js";
+export { default as CustomerCard } from "./CustomerCard";
 
 
 
