a72feff5f99e788ddfb40f5a0d416e00c744bacf
Jordan Davidson <jordandavidson744@yahoo.com>
2020-01-13 08:40:40 -0500

    updated paths

---
+++ src/react/pages/MyRequests.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class MyRequests extends React.Component {
+  render(){
+    return (
+        <p>This is the requests page</p>
+    );
+  }
+}
+
+export default MyRequests
---
+++ src/react/pages/ViewRequests.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ViewRequests extends React.Component {
+  render(){
+    return (
+        <p>This is the requests page</p>
+    );
+  }
+}
+
+export default ViewRequests
