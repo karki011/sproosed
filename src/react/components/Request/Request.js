798708b5f79882aec314ca1a7fc6494a7a25219e
DavidRinSE <davidrichardsonin@gmail.com>
2020-01-14 12:53:48 -0500

    Fixing Dietrich's broken shit.

--- src/react/components/Request/Request.js
+++ src/react/components/Request/Request.js
@@ -3,40 +3,44 @@ import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import "./Request.css";
 import { BidsList } from "../index";
-import {connect} from "react-redux"
-import {Redirect} from 'react-router-dom'
+import { connect } from "react-redux"
+import { Redirect } from 'react-router-dom'
 import * as actionCreators from "../../../redux/actionCreators"
 import moment from "moment";
 
 class Request extends React.Component {
-  componentDidMount(){
-    if(this.props.loginState.result){
+  componentDidMount() {
+    if (this.props.loginState.result) {
       this.props.getRequests()
     }
   }
   render() {
-    if(!this.props.loginState.result){
+    if (!this.props.loginState.result) {
       return (
         <Redirect to="/" />
       )
     }
     if (this.props.getRequestsState.result === null) {
+      //Loading
       return (
-<h1>hello</h1>
+        <h1>hello</h1>
       );
     }
-const getRequest = this.props.getRequestsState.result.requests
-return getRequest.map(request => {
-    return (
-      
-      <div className="requestwrapper">
-        <CardContent>
-          <Typography style={{ fontSize: "21px" }}>{request.text}</Typography>
-          <Typography style={{ fontSize: "19px" }} color="textSecondary">
-          {moment(request.createdAt).format("MMM Do YYYY")}
-          </Typography>
+    if (this.props.getRequestsState.result.requests.length === 0){
+      //No Requests
+      return <h1>No requests found!</h1>
+    }
+    const getRequest = this.props.getRequestsState.result.requests
+    return getRequest.map(request => {
+      return (
 
-            <BidsList bids={request.bids} />
+        <div className="requestwrapper">
+          <CardContent>
+            <Typography style={{ fontSize: "21px" }}>{request.text}</Typography>
+            <Typography style={{ fontSize: "19px" }} color="textSecondary">
+              {moment(request.createdAt).format("MMM Do YYYY")}
+            </Typography>
+            {request.bids.length > 0 && <BidsList bids={request.bids} />}
           </CardContent>
         </div>
       );
@@ -44,10 +48,10 @@ return getRequest.map(request => {
   }
 }
 
-function mapStateToProps(state){
-  let getRequests = {...state["requests"]["getRequests"]}
-  let login = {...state["auth"]["login"]}
-  return {getRequestsState: getRequests, loginState: login}
+function mapStateToProps(state) {
+  let getRequests = { ...state["requests"]["getRequests"] }
+  let login = { ...state["auth"]["login"] }
+  return { getRequestsState: getRequests, loginState: login }
 }
 let mapDispatchToProps = {
   getRequests: actionCreators["getRequests"]
--- src/react/pages/Home.js
+++ src/react/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavBar } from "../components"
 import {Request} from "../components/index"
+import {NavLink} from "react-router-dom"
 import "./Home.css"
 
 class Home extends React.Component {
@@ -14,7 +15,7 @@ class Home extends React.Component {
               
                 <Request />
 
-                <a href=" " className="floating-btn">+</a>
+                <NavLink className="floating-btn" to="/request-services">+</NavLink>
       </div>
 
       </>
