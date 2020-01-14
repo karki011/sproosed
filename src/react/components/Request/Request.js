cb94cb515d782233e4ac46df299669f79c03c7c4
Detrich Schilling <detrich2k@gmail.com>
2020-01-14 14:53:55 -0500

    fixed some formatting

--- src/react/components/Request/Request.js
+++ src/react/components/Request/Request.js
@@ -3,45 +3,40 @@ import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import "./Request.css";
 import { BidsList } from "../index";
-import { connect } from "react-redux"
-import { Redirect } from 'react-router-dom'
-import * as actionCreators from "../../../redux/actionCreators"
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
+import * as actionCreators from "../../../redux/actionCreators";
 import moment from "moment";
-import {trees,arrow} from "../../images"
+import { trees, arrow } from "../../images";
 
 class Request extends React.Component {
   componentDidMount() {
     if (this.props.loginState.result) {
-      this.props.getRequests()
+      this.props.getRequests();
     }
   }
   render() {
     if (!this.props.loginState.result) {
-      return (
-        <Redirect to="/" />
-      )
+      return <Redirect to="/" />;
     }
     if (this.props.getRequestsState.result === null) {
       //Loading
-      return (
-        <h1>hello</h1>
-      );
+      return <h1>hello</h1>;
     }
-    if (this.props.getRequestsState.result.statusCode === 404){
-      //No Requests
+    if (this.props.getRequestsState.result.statusCode === 404) {
+      //No Requestsgit 
       return (
         <div>
-        <div className="afterLoginPage">Sproose up your yard</div>
-      <img src={trees} alt="trees" className="trees" />
-      <div className= "afterLoginPage">Request a service</div> 
-      <img src={arrow} alt="arrow" className="arrow"/>
-      </div>
-      )
+          <div className="afterLoginPage">Sproose up your yard</div>
+          <img src={trees} alt="trees" className="trees" />
+          <div className="afterLoginPage">Request a service</div>
+          <img src={arrow} alt="arrow" className="arrow" />
+        </div>
+      );
     }
-    const getRequest = this.props.getRequestsState.result.requests
+    const getRequest = this.props.getRequestsState.result.requests;
     return getRequest.map(request => {
       return (
-
         <div className="requestwrapper">
           <CardContent>
             <Typography style={{ fontSize: "21px" }}>{request.text}</Typography>
@@ -57,9 +52,9 @@ class Request extends React.Component {
 }
 
 function mapStateToProps(state) {
-  let getRequests = { ...state["requests"]["getRequests"] }
-  let login = { ...state["auth"]["login"] }
-  return { getRequestsState: getRequests, loginState: login }
+  let getRequests = { ...state["requests"]["getRequests"] };
+  let login = { ...state["auth"]["login"] };
+  return { getRequestsState: getRequests, loginState: login };
 }
 let mapDispatchToProps = {
   getRequests: actionCreators["getRequests"]
