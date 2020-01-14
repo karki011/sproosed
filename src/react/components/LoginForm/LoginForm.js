917d0b18f898919154a2936f46542727c0c86162
Jordan Davidson <jordandavidson744@yahoo.com>
2020-01-14 12:24:17 -0500

    started styling loginForm

--- src/react/components/LoginForm/LoginForm.js
+++ src/react/components/LoginForm/LoginForm.js
@@ -2,15 +2,11 @@ import React from "react";
 import {
   Container,
   CssBaseline,
-  Avatar,
-  Typography,
   TextField,
   FormControlLabel,
   Checkbox,
   Button,
-  Grid,
   Box,
-  Link,
 } from "@material-ui/core";
 // import Link from "."; Will need to switch this to react-router later
 import { makeStyles } from '@material-ui/core/styles';
@@ -57,12 +53,6 @@ class LoginForm extends React.Component {
     return (
       <Container component="main" maxWidth="xs">
         <CssBaseline />
-        <div className={useStyles.paper}>
-          <Avatar className={useStyles.avatar}>
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
           <form className={useStyles.form} onSubmit={this.handleLogin} noValidate>
             <TextField
               variant="outlined"
@@ -101,22 +91,9 @@ class LoginForm extends React.Component {
             >
               Sign In
             </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="/register" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
           </form>
           {(this.props.loginState && this.props.loginState.result && this.props.loginState.result.statusCode === 400) && <h3>{this.props.loginState.result.message}</h3>}
           {(this.props.loginState && this.props.loginState.result && this.props.loginState.result.statusCode === 200) && <Redirect to="/home"/>}
-        </div>
         <Box mt={8}>
           <Copyright />
         </Box>
